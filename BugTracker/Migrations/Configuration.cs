ec860e05f079577149f4e088e1789d2b8650c4cd
Dilraj Singh <dilrajkehra@gmail.com>
2019-04-03 09:26:19 -0500

    edited layout page

---
+++ BugTracker/Migrations/Configuration.cs
@@ -0,0 +1,89 @@
+namespace BugTracker.Migrations
+{
+    using BugTracker.Models;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<BugTracker.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "Blog.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(BugTracker.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+
+            //role manager
+            var roleManager =
+               new RoleManager<IdentityRole>(
+                   new RoleStore<IdentityRole>(context));
+
+            //Admin role
+            if (!context.Roles.Any(p => p.Name == "Admin"))
+            {
+                var adminRole = new IdentityRole("Admin");
+                roleManager.Create(adminRole);
+            }
+
+            //Project Manager role
+            if (!context.Roles.Any(p => p.Name == "Project Manager"))
+            {
+                var projectManagerRole = new IdentityRole("Project Manager");
+                roleManager.Create(projectManagerRole);
+            }
+
+            //Developer role
+            if (!context.Roles.Any(p => p.Name == "Developer"))
+            {
+                var developerRole = new IdentityRole("Developer");
+                roleManager.Create(developerRole);
+            }
+
+            //Submitter role
+            if (!context.Roles.Any(p => p.Name == "Submitter"))
+            {
+                var submitterRole = new IdentityRole("Submitter");
+                roleManager.Create(submitterRole);
+            }
+
+            //user manager
+            var userManager =
+                new UserManager<ApplicationUser>(
+                        new UserStore<ApplicationUser>(context));
+
+            //Creating admin user
+            ApplicationUser admin;
+
+            if (!context.Users.Any(
+                p => p.UserName == "admin@mybugtracker.com"))
+            {
+                admin = new ApplicationUser();
+                admin.UserName = admin.Email = "admin@mybugtracker.com";
+
+                userManager.Create(admin, "Password-1");
+            }
+            else
+            {
+                admin = context
+                    .Users
+                    .First(p => p.UserName == "admin@mybugtracker.com");
+            }
+
+            //assigning admin role if not assigned
+            if (!userManager.IsInRole(admin.Id, "Admin"))
+            {
+                userManager.AddToRole(admin.Id, "Admin");
+            }
+        }
+    }
+}
