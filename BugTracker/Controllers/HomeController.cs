cebcf06946e746f307c6b8838b7e71b27d767073
Dilraj Singh <dilrajkehra@gmail.com>
2019-04-05 17:27:33 -0500

    Added functionality to assign or unassign members to a projects and some UI changes on Index.html

--- BugTracker/Controllers/HomeController.cs
+++ BugTracker/Controllers/HomeController.cs
@@ -189,11 +189,133 @@ namespace BugTracker.Controllers
             return View(projects);
         }
 
+        [Authorize(Roles = "Admin, Project Manager")]
+        [HttpGet]
+        public ActionResult EditProject(int? id)
+        {
+            if (!id.HasValue)
+            {
+                return RedirectToAction(nameof(HomeController.AllProjects));
+            }
+
+            HomeProjectViewModel project = DbContext.Projects
+                .Where(p => p.Id == id)
+                .Select(p => new HomeProjectViewModel
+                {
+                    Name = p.Name,
+                }).ToList().FirstOrDefault();
+
+            return View(project);
+        }
+
+        [Authorize(Roles = "Admin, Project Manager")]
+        [HttpPost]
+        public ActionResult EditProject(HomeProjectViewModel model, int? id)
+        {
+            if (!id.HasValue)
+            {
+                return RedirectToAction(nameof(HomeController.AllProjects));
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            Project project = DbContext.Projects
+                .FirstOrDefault(p => p.Id == id);
+
+            if (project == null)
+            {
+                return RedirectToAction(nameof(HomeController.AllProjects));
+            }
+
+            project.Name = model.Name;
+            project.DateUpdated = DateTime.Now;
+
+            DbContext.SaveChanges();
+
+            return RedirectToAction(nameof(HomeController.AllProjects));
+        }
+
+        [Authorize(Roles = "Admin, Project Manager")]
+        public ActionResult DeleteProject(int? id)
+        {
+            if (!id.HasValue)
+            {
+                return RedirectToAction(nameof(HomeController.AllProjects));
+            }
+
+            Project project = DbContext.Projects
+                .FirstOrDefault(p => p.Id == id);
+
+            if (project == null)
+            {
+                return RedirectToAction(nameof(HomeController.AllProjects));
+            }
+
+            DbContext.Projects.Remove(project);
+
+            DbContext.SaveChanges();
+
+            return RedirectToAction(nameof(HomeController.AllProjects));
+        }
+
+        [Authorize(Roles = "Admin, Project Manager")]
+        [HttpGet]
+        public ActionResult AllUsers(AssignProjectMembersViewModel model, int? id)
+        {
+            if (!id.HasValue)
+            {
+                return RedirectToAction(nameof(HomeController.AllProjects));
+            }
+            else
+            {
+                model.Id = id;
+                model.Users = DbContext.Users.ToList();
+
+                return View(model);
+            }
+        }
+
+        [Authorize(Roles = "Admin, Project Manager")]
+        [HttpPost]
+        public ActionResult AssignProject(int? projectId, string userId, bool add)
+        {
+            if (!projectId.HasValue || string.IsNullOrEmpty(userId))
+            {
+                return RedirectToAction(nameof(HomeController.AllProjects));
+            }
+            else
+            {
+                Project project = DbContext.Projects.
+                    FirstOrDefault(p => p.Id == projectId);
+
+                ApplicationUser user = DbContext.Users
+                .FirstOrDefault(p => p.Id == userId);
+
+                if (add)
+                {
+                    user.Projects.Add(project);
+                }
+                else
+                {
+                    user.Projects.Remove(project);
+                }
+
+                DbContext.SaveChanges();
+
+                return RedirectToAction(nameof(HomeController.AllUsers));
+            }
+        }
+
         [HttpGet]
         public ActionResult MyProjects()
         {
             string userId = User.Identity.GetUserId();
-            List<HomeProjectViewModel> projects = DbContext.Projects.Where(p => p.Users.Any(x => x.Id == userId)).Select(p =>
+            List<HomeProjectViewModel> projects = DbContext.Projects
+                .Where(p => p.Users.Any(x => x.Id == userId))
+                .Select(p =>
                 new HomeProjectViewModel
                 {
                     Id = p.Id,
---
+++ BugTracker/Models/ViewModels/AssignProjectMembersViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BugTracker.Models.ViewModels
+{
+    public class AssignProjectMembersViewModel
+    {
+        public int? Id { get; set; }
+        public List<ApplicationUser> Users { get; set; }
+
+        public AssignProjectMembersViewModel()
+        {
+            Users = new List<ApplicationUser>();
+        }
+    }
+}
