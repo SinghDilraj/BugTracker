bd2adbd92e339e8fcc7e73e08b79ce408aad032c
Dilraj Singh <dilrajkehra@gmail.com>
2019-04-16 12:55:19 -0500

    bug fix in edit ticket action authorization

--- BugTracker/Controllers/TicketsController.cs
+++ BugTracker/Controllers/TicketsController.cs
@@ -127,7 +127,6 @@ namespace BugTracker.Controllers
             }
         }
 
-        [Authorize(Roles = "Admin, Project Manager")]
         [HttpGet]
         public ActionResult EditTicket(int? ticketId)
         {
